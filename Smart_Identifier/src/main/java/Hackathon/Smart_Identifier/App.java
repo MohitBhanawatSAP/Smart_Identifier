6b27cb3e7a78af4fd367c917c19a2907ac6632bc
C5281054 <mohit.bhanawat@sap.com>
2019-12-10 12:57:47 +0530

    .

---
+++ Smart_Identifier/src/main/java/Hackathon/Smart_Identifier/App.java
@@ -0,0 +1,14 @@
+package Hackathon.Smart_Identifier;
+
+import java.io.File;
+import java.net.Socket;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+}
